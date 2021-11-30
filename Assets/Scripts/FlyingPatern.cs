d9d5ca7d3e99e04d9c5dbacab13e017d3c05829e
Joost-Robat <30769@ma-web.nl>
2021-11-30 20:22:33 +0100

    Errors opgelost.

--- Assets/Scripts/FlyingPatern.cs
+++ Assets/Scripts/FlyingPatern.cs
@@ -7,7 +7,7 @@ public class FlyingPatern : MonoBehaviour
     [SerializeField] private float maxVelocity = 0.1f;
     [SerializeField] private float maxMinusVelocity = -0.1f;
     private float startX, startY, velocityY, velocityX;
-    private float multiplier = 0.1f;
+    private float multiplier = 1f;
     private Rigidbody2D body;
     [SerializeField]private GameObject Firefly;
 
@@ -21,10 +21,62 @@ public class FlyingPatern : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position += new Vector3(velocityX, velocityY, 0);
+        gameObject.transform.position += new Vector3(velocityX * Time.deltaTime, velocityY * Time.deltaTime, 0);
         if(gameObject.transform.position.x > startX)
+        {
+            if(velocityX < 0)
+            {
+                CalculateMultiplier(velocityX);
+                velocityX += Time.deltaTime * multiplier;
+            }
+            else
+            {
+                velocityX += Time.deltaTime;
+            }
+        } // deze
+        else
+        {
+            if(velocityX > 0)
+            {
+                CalculateMultiplier(velocityX);
+                velocityX -= Time.deltaTime * multiplier;
+            }
+            else
+            {
+                velocityX -= Time.deltaTime;
+            }
+        }
+        if(gameObject.transform.position.y > startY)
+        {
+            if(velocityY < 0)
+            {
+                CalculateMultiplier(velocityY);
+                velocityY += Time.deltaTime * multiplier;
+            }
+            else
+            {
+                velocityY += Time.deltaTime;
+            }
+        } // deze
+        else
+        {
+            if(velocityY > 0)
+            {
+                CalculateMultiplier(velocityY);
+                velocityY -= Time.deltaTime * multiplier;
+            }
+            else
+            {
+                velocityY -= Time.deltaTime;
+            }
+        }
     }
-    public void CalculateMultiplier()
+    public void CalculateMultiplier(float speed)
+    {
+        multiplier = System.Math.Abs(speed / 4);
+        multiplier++;
+    }
+    public void randomDirection()
     {
 
     }
