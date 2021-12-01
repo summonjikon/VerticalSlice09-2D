a4de91258a1fd10b701418e8665e784d8806ab75
Joost-Robat <30769@ma-web.nl>
2021-12-01 15:27:03 +0100

    Stevig begin gemaakt aan de flocking AI van de vuurvliegjes.

---
+++ Assets/Scripts/FlockUnit.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockUnit : MonoBehaviour
+{
+    [SerializeField] private float FOVAngle;
+    [SerializeField] private float smoothDamp;
+    private List<FlockUnit> cohesionNeighbours = new List<FlockUnit>();
+    private Flock assignedFlock;
+    private Vector3 currentVelocity;
+    private float speed;
+    private Flock test;
+
+    public Transform myTransform { get; set; }
+    private void Awake()
+    {
+        myTransform = transform;
+        test = GetComponent<Flock>();
+    }
+    public void AssignFlock(Flock flock)
+    {
+        assignedFlock = flock;
+    }
+
+    public void InitializeSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+    private void Update()
+    {
+        for(int i = 0; i < test.allUnits.Length; i++)
+        {
+            test.allUnits[i].MoveUnit();
+        }
+    }
+    public void MoveUnit()
+    {
+        FindNeighbours();
+        var cohesionVector = CalculateCohesionVector();
+        var moveVector = Vector3.SmoothDamp(myTransform.forward, cohesionVector, ref currentVelocity, smoothDamp);
+        moveVector = moveVector.normalized * speed;
+        myTransform.forward = moveVector;
+        myTransform.position += moveVector * Time.deltaTime;
+    }
+    private void FindNeighbours()
+    {
+        cohesionNeighbours.Clear();
+        var allUnits = assignedFlock.allUnits;
+        for(int i = 0; i < allUnits.Length; i++)
+        {
+            var currentUnit = allUnits[i];
+            if(currentUnit != this)
+            {
+                float currentNeightbourDistanceSqr = Vector3.SqrMagnitude(currentUnit.myTransform.position - myTransform.position);
+                if(currentNeightbourDistanceSqr <= assignedFlock.cohesionDistance * assignedFlock.cohesionDistance)
+                {
+                    cohesionNeighbours.Add(currentUnit);
+                }
+            }
+            
+        }
+    }
+    private Vector3 CalculateCohesionVector()
+    {
+        var cohesionVector = Vector3.zero;
+        if (cohesionNeighbours.Count == 0)
+            return cohesionVector;
+        int neighboursInFOV = 0;
+        for(int i = 0; i < cohesionNeighbours.Count; i++)
+        {
+            if (IsInFOV(cohesionNeighbours[i].myTransform.position))
+            {
+                neighboursInFOV++;
+                cohesionVector += cohesionNeighbours[i].myTransform.position;
+            }
+        }
+        if (neighboursInFOV == 0)
+            return cohesionVector;
+        cohesionVector /= neighboursInFOV;
+        cohesionVector -= myTransform.position;
+        cohesionVector = Vector3.Normalize(cohesionVector);
+        return cohesionVector;
+    }
+    private bool IsInFOV(Vector3 position)
+    {
+        return Vector3.Angle(myTransform.forward, position - myTransform.position) <= FOVAngle;
+    }
+}
